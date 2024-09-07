1f2b54bb1f377bb17589022d605fa71cc215ce1b
Asad Aslam <opworrier@gmail.com>
2024-09-07 15:34:29 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,140 @@
+// toggle button
+const navMenu = document.getElementById("nav-menu")
+const navLink = document.querySelectorAll(".nav-link")
+const hamburger = document.getElementById("hamburger")
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("left-[0]")
+    hamburger.classList.toggle('ri-close-large-line')
+})
+
+navLink.forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.toggle("left-[0]")
+        hamburger.classList.toggle('ri-close-large-line')
+    })
+})
+
+// Show scroll up
+const scrollUp = () => {
+    const scrollUpBtn = document.getElementById("scroll-up")
+
+    if (this.scrollY >= 250) {
+        scrollUpBtn.classList.remove("-bottom-1/2")
+        scrollUpBtn.classList.add("bottom-4")
+    } else {
+        scrollUpBtn.classList.add("-bottom-1/2")
+        scrollUpBtn.classList.remove("bottom-4")
+    }
+}
+window.addEventListener("scroll", scrollUp)
+
+
+// Change background header
+const scrollHeader = () => {
+    const header = document.getElementById("navbar")
+
+    if (this.scrollY >= 50) {
+        header.classList.add("border-b", "border-yellow-500")
+    } else {
+        header.classList.remove("border-b", "border-yellow-500")
+    }
+}
+window.addEventListener("scroll", scrollHeader)
+
+
+// Swiper
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    speed: 400,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    grabCursor: true,
+    breakpoints: {
+        640: {
+            slidesPerView: 1
+        },
+        760: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+    }
+});
+
+
+// Scroll sections active link
+const activeLink = () => {
+    const section = document.querySelectorAll('section')
+    const navlinks = document.querySelectorAll(".nav-link")
+
+    let current = "home"
+
+    section.forEach(section => {
+        const sectionTop = section.offsetTop;
+
+        if (this.scrollY >= sectionTop - 60) {
+            current = section.getAttribute("id")
+        }
+    })
+
+    navlinks.forEach(item => {
+        item.classList.remove("active")
+        if (item.href.includes(current)) {
+            item.classList.remove("active")
+
+        }
+    })
+}
+
+window.addEventListener("scroll", activeLink)
+
+
+// scroll reveal Animation
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "60px",
+    duration: 2500,
+    delay: 300,
+    reset: true
+})
+
+sr.reveal(`.Home__data, .about__top, .popular__top, .review__top, .review__swiper, .footer__icon, .footer__content, .copy__right`)
+sr.reveal(`.home__image`, { delay: 500, scale: 0.5 })
+
+sr.reveal(`.service__card, .popular__card`, { interval: 100, })
+
+sr.reveal(`.about__leaf`, { delay: 1000, origin: "right" })
+sr.reveal(`.about__item__1-content, .about__item__2-img`, { origin: "right" })
+sr.reveal(`.about__item__2-content, .about__item__1-img`, { origin: "left" })
+
+sr.reveal(`.review__leaf, .footer__floral`, { delay: 1000, origin: "left" })
+
+
+// emailjs configuration
+function sendMail() {
+    var params = {
+        email: document.getElementById("email").value,
+    };
+
+    const serviceID = "service_qk25k2e";
+    const templateID = "template_cob1ilm"
+
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            document.getElementById("email").value = "";
+            console.log(res);
+            alert("your message has sent sucessfully")
+        })
+        .catch((error) => console.log(error));
+}
